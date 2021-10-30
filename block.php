4aa047ebe8a416d0dca64ac3e8f66750bc581a4f
Zech Bron <39665654+ZechBron@users.noreply.github.com>
2021-10-30 17:28:57 +0700

    Create block.php

---
+++ block.php
@@ -0,0 +1,35 @@
+<?php
+/* ---[ Block both IP & User Agent after 2 to 3 visits ]--- */
+@session_start();
+
+if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+}
+elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+}
+elseif (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+        $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+}
+else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+}
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+
+if (isset($_SESSION['first_visit'])) {
+   if (isset($_SESSION['second_visit'])) {
+      $zCh = fopen("blocked_ip.txt", "a");
+      fwrite($zCh, $ip . "\n");
+      fclose($zCh);
+      $ChB = fopen("blocked_ua.txt", "a");
+      fwrite($ChB, $user_agent . "\n");
+      fclose($ChB);
+   }
+   else {
+      $_SESSION['second_visit'] = "Second Visit";
+   }
+}
+else {
+   $_SESSION['first_visit'] = "First Visit";
+}
+?>
