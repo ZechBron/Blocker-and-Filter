4111b7910398ee8e2a34ed7663bcadaa182fa1f6
Zech Bron <39665654+ZechBron@users.noreply.github.com>
2022-04-28 14:25:34 +0700

    Update country_filter.php

--- country_filter.php
+++ country_filter.php
@@ -2,8 +2,25 @@
 error_reporting(0);
 error_log(1);
 
-$ip = file_get_contents("https://ipapi.co/ip");
+if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+	$ip = $_SERVER['HTTP_CLIENT_IP'];
+}
+elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+	$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+}
+elseif (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+	$ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+}
+else {
+	$ip = $_SERVER['REMOTE_ADDR'];
+}
+
 $country = file_get_contents("https://ipapi.co/" . $ip . "/country_name");
+
+$z = fopen("blocked.txt", "a");
+fwrite($z, "IP Addr: " . $ip . "\n");
+fwrite($z, "Country: " . $country . "\n\n");
+fclose($z);
 
 if ($country != "Philippines") {
 header("HTTP/1.0 404 Not Found");
